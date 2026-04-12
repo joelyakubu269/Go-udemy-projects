d0a01970e06ad0e17d9d7484e9568b6f03a933db
Stanley-0ps <ochaistanley7@gmail.com>
2026-04-12 16:01:38 +0100

    a simple investment calculator

--- investmentcalculator.go
+++ investmentcalculator.go
@@ -1,8 +1,11 @@
 package main
+import(
+	"math"
+)
 
 func main() {
 	var investedAmount = 1000
 	var expectedReturnRate = 5.5
 	var years = 10
-	var futureValue = float64(investedAmount) * (1 + expectedReturnRate/100)
+	var futureValue = float64(investedAmount) * math.Pow(1 + expectedReturnRate/100, float64(years))
 }
